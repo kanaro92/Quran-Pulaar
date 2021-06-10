389c6a78b5a3a025400573cd13af824712a16e4f
Kane Hamath <kanaro1992@gmail.com>
2021-06-10 12:05:38 +0000

    update changes

---
+++ android/app/src/main/java/mr/quran_teddudo_pulaar/MainActivity.java
@@ -0,0 +1,15 @@
+package mr.quran_teddudo_pulaar;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "quran-pulaar";
+  }
+}
