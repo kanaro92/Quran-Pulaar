8f0db1cda6e57dfc4a72c5c3e756f8703f55ff2d
Kane Hamath <hamath.kane@adias.fr>
2022-01-01 14:57:16 +0000

    refresh project

--- ios/Pods/Target Support Files/Pods-Quran-tvOS/Pods-Quran-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Quran_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_Quran_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-Quran-tvOSTests/Pods-Quran-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Quran_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Quran_tvOSTests
-@end
